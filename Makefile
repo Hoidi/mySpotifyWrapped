8d9ff3b5a20ca3504f63916919d6752b07be22ff
Hoidi <tobiakar@student.chalmers.se>
2021-04-11 11:35:10 +0200

    Added Makefile with arguments

---
+++ Makefile
@@ -0,0 +1,24 @@
+general:
+	go build main.go helper.go setup.go stats.go
+	./main general
+	rm -f main helper
+
+artistUnique:
+	go build main.go helper.go setup.go stats.go
+	./main artist unique
+	rm -f main helper
+
+artistTime:
+	go build main.go helper.go setup.go stats.go
+	./main artist time
+	rm -f main helper
+
+trackUnique:
+	go build main.go helper.go setup.go stats.go
+	./main track unique
+	rm -f main helper
+
+trackTime:
+	go build main.go helper.go setup.go stats.go
+	./main track time
+	rm -f main helper
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 )
 
 type SongPlayJSON struct {
@@ -19,34 +20,78 @@ var songTimeListens map[string]int		// all songs and how many milli seconds they
 func main() {
 	Setup()
 
+	args := os.Args
+
+	switch args[1]{
+	case "general":
+		printGeneral()
+	case "artist":
+		if args[2] == "unique" {
+			printArtistUnique()
+		} else if args[2] == "time" {
+			printArtistTime()
+		}
+	case "track":
+		if args[2] == "unique" {
+			printTrackUnique()
+		} else if args[2] == "time" {
+			printTrackTime()
+		}
+	default:
+		fmt.Print("Oh no")
+	}
+
+}
+
+func printGeneral() {
 	fmt.Print(TotalTimeListened()," hours of total listening time.\n\n")
 
 	uniqueArtists, uniqueTracks := NumberOfUnique()
 	fmt.Print("You listened to ", uniqueArtists, " unique artist and ",uniqueTracks, " unique tracks.\n\n")
 
-	popArtist, popArtistListens := MostPopularArtist(true)
-	fmt.Print(popArtist," is the artist you've listened to the most times. Total listening times is ", popArtistListens,".\n")
-	popArtist, popArtistListens = MostPopularArtist(false)
+
+	printHours(true)
+}
+
+func printArtistTime() {
+	popArtist, popArtistListens := MostPopularArtist(false)
 	fmt.Print(popArtist," is the artist you've listened to the most time. Total listening time is ", popArtistListens," hours.\n\n")
 
-	popTrack, popTrackListens := MostPopularTrack(true)
-	fmt.Print(popTrack," is the track you've listened to the most times. Total listening times is ", popTrackListens,".\n")
-	popTrack, popTrackListens = MostPopularTrack(false)
-	fmt.Print(popTrack," is the track you've listened to the most time. Total listening time is ", popTrackListens," hours.\n\n")
-
-	streakArtist, streakNum := HighestSteakTrack(true)
-	fmt.Print(streakArtist, " is the song with the highest streak in a row. The streak was ", streakNum,"\n")
-	streakTrackTime, streakNumTime := HighestSteakTrack(false)
-	fmt.Print(streakTrackTime, " is the song with the longest streak in a row. The streak time was ", streakNumTime, " minutes.\n\n")
-
-	streakArtist, streakArtistNum := HighestSteakArtist(true)
-	fmt.Print(streakArtist, " is the artist with the highest streak in a row. The streak was ", streakArtistNum,"\n")
 	streakArtistTime, streakArtistNumTime := HighestSteakArtist(false)
 	fmt.Print(streakArtistTime, " is the artist with the longest streak in a row. The streak time was ", streakArtistNumTime, " minutes.\n\n")
 
 	printTopN(10,"tracks",false)
+}
 
-	printTopN(10,"artists",false)
+func printArtistUnique() {
+	popArtist, popArtistListens := MostPopularArtist(true)
+	fmt.Print(popArtist," is the artist you've listened to the most times. Total listening times is ", popArtistListens,".\n\n")
+
+	streakArtist, streakArtistNum := HighestSteakArtist(true)
+	fmt.Print(streakArtist, " is the artist with the highest streak in a row. The streak was ", streakArtistNum,"\n\n")
+
+	printTopN(10,"tracks",true)
+}
+
+func printTrackTime() {
+	popTrack, popTrackListens := MostPopularTrack(false)
+	fmt.Print(popTrack," is the track you've listened to the most time. Total listening time is ", popTrackListens," hours.\n\n")
+
+	streakTrackTime, streakNumTime := HighestSteakTrack(false)
+	fmt.Print(streakTrackTime, " is the song with the longest streak in a row. The streak time was ", streakNumTime, " minutes.\n\n")
+
+	printTopN(10,"tracks",false)
+}
+
+func printTrackUnique() {
+	popTrack, popTrackListens := MostPopularTrack(true)
+	fmt.Print(popTrack," is the track you've listened to the most times. Total listening times is ", popTrackListens,".\n\n")
+
+	streakTrack, streakTrackNum := HighestSteakTrack(true)
+	fmt.Print(streakTrack, " is the song with the highest streak in a row. The streak was ", streakTrackNum,"\n\n")
+
+
+	printTopN(10,"tracks",true)
 }
 
 func printTopN(n int, a string, unique bool) {
@@ -58,7 +103,20 @@ func printTopN(n int, a string, unique bool) {
 	}
 	fmt.Print("Your top ", n, " ", a,".\n")
 	for i, item := range topList[:n] {
-		fmt.Print(i+1,". ", item.Key, ": ", item.Value, " minutes. (", item.Value/60," hours)\n")
+		if unique {
+			fmt.Print(i+1,". ", item.Key, ": ", item.Value, " minutes. (", item.Value/60," hours)\n")
+		} else {
+			fmt.Print(i+1,". ", item.Key, ": ", item.Value, " times.\n")
+		}
+
 	}
 	fmt.Print("\n")
+}
+
+func printHours(unique bool) {
+	var hours [24]int = Hour(unique)
+
+	for i,j := range hours {
+		fmt.Print(i," ",j,"\n")
+	}
 }
